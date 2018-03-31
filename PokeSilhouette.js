81e9cc557fb2d75cf09bcdb42f961ff2433d9d42
MrLuxan <email@Robert-Walker.com>
2018-03-31 23:48:14 +0100

    day 2 submit

--- PokeSilhouette.js
+++ PokeSilhouette.js
@@ -17,7 +17,7 @@ var PokeSilhouette = (function() {
       	callBack(JSON.parse(this.responseText));       
     	}
   	};
-  	xhttp.open('GET', 'https://api.pokemontcg.io/v1/cards?name= '+ cardName, true);
+  	xhttp.open('GET', 'https://api.pokemontcg.io/v1/cards?name='+ cardName, true);
   	xhttp.send();
   }
   
@@ -35,14 +35,43 @@ var PokeSilhouette = (function() {
           for(let x = 0; x < library.cardArray[y].length; x++)
               ctx.drawImage(library.cardArray[y][x], xStart + x*cardWidth, yStart + y*cardHeight,cardWidth,cardHeight);    
 
-      return c.toDataURL();
+      return c;
+  }
+
+  library.DrawSilhouette = function(mask,offSetX, offSetY)
+  {
+    var c = document.createElement('canvas');
+    c.width = 800;
+    c.height = 800;
+
+    var ctx=c.getContext("2d");
+    ctx.clearRect(0,0,c.width,c.height);
+ 
+    let img = PokeSilhouette.DrawCardArray(offSetX, offSetY);
+    ctx.drawImage(mask, 0, 0, 600 , 530);
+    ctx.globalCompositeOperation = 'source-in';
+    ctx.drawImage(img, 0 , 0);
+
+    return c;
   }
 
 
-	library.GenBackGround = function(cardName)
+	library.FindCards = function(cardName)
   {
-		  GetCardData(cardName,function(data){
-        console.log(data);
+		  GetCardData(cardName,function(data){        
+
+        let outputDiv = document.getElementById("findCards");
+        outputDiv.innerHTML = "";
+
+        data.cards.forEach(function(element) 
+        {
+          console.log(element);
+          let image = document.createElement("img");
+          image.setAttribute("src", element.imageUrl);
+          outputDiv.appendChild(image);
+        });
+
+
       });
   }
 

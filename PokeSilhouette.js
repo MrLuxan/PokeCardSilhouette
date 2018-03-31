8c0115e5c51586c79c31c3396370ddc8c9bf746e
MrLuxan <email@Robert-Walker.com>
2018-03-31 18:31:00 +0100

    Initial commit
    First commit point

---
+++ PokeSilhouette.js
@@ -0,0 +1,51 @@
+var PokeSilhouette = (function() {
+
+  let cardWidth = 122.5;
+  let cardHeight = 171;
+
+	let library = {};
+
+  library.cardArray = [];
+
+	function GetCardData(cardName,callBack)
+  {
+  	var xhttp = new XMLHttpRequest();
+  	xhttp.onreadystatechange = function()
+    {
+    	if (this.readyState == 4 && this.status == 200)
+      {
+      	callBack(JSON.parse(this.responseText));       
+    	}
+  	};
+  	xhttp.open('GET', 'https://api.pokemontcg.io/v1/cards?name= '+ cardName, true);
+  	xhttp.send();
+  }
+  
+  library.DrawCardArray = function(offSetX, offSetY)
+  {
+      let xStart = (offSetX ? - cardWidth/2 : 0);
+      let yStart = (offSetY ? - cardHeight/2 : 0);
+
+      let c=document.createElement('canvas');
+      c.width = 800;
+      c.height = 800;
+
+      let ctx=c.getContext("2d");
+      for(let y = 0; y < library.cardArray.length; y++)
+          for(let x = 0; x < library.cardArray[y].length; x++)
+              ctx.drawImage(library.cardArray[y][x], xStart + x*cardWidth, yStart + y*cardHeight,cardWidth,cardHeight);    
+
+      return c.toDataURL();
+  }
+
+
+	library.GenBackGround = function(cardName)
+  {
+		  GetCardData(cardName,function(data){
+        console.log(data);
+      });
+  }
+
+  return library;
+  
+})();
---
+++ README.md
@@ -0,0 +1,11 @@
+# PokeCardSilhouette
+
+Project for reddit to help with current silhouette pictures with poekmon cards.
+
+E.g. 
+![alt text](https://i.imgur.com/y28Z2Nk.jpg "Logo Title Text 1")
+[More by u/SassyButtDragon](https://www.reddit.com/r/pokemon/comments/7y95m7/after_we_posted_our_original_blastoise_silhouette/?st=jfd9armd&sh=29511378)
+
+
+---
+Uses [Pokémon TCG API](https://pokemontcg.io/)

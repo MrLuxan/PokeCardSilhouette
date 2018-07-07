089fa0fc1ab2842e33fa2a2ad3b6656e9424ce7d
MrLuxan <email@Robert-Walker.com>
2018-07-07 22:45:50 +0100

    update

--- PokeSilhouette.js
+++ PokeSilhouette.js
@@ -2,24 +2,41 @@ var PokeSilhouette = (function() {
 
   let library = {};
 
-  let cardWidth = 122.5;
-  let cardHeight = 171;
+  let CardSize = {
+  	Width: 245,
+  	Height: 342,
+  	CardScale: 0.5,
+  	get Size() { return {Width : this.Width , Height : this.Height}; },
+  	get DrawSize() { return {Width : this.Width * this.CardScale , Height : this.Height * this.CardScale}; }
+  }
+
+  let UI = {
+  	get CardsWide(){return document.getElementById("cardsxPicker").value;},
+  	get CardsHeigh(){return document.getElementById("cardsyPicker").value;},
+    get CardsxOffest(){return document.getElementById("cardsxOffest").value;},
+    get CardsyOffest(){return document.getElementById("cardsyOffest").value;},
+  	get ShowForground(){return document.getElementById("showForeground").checked;},
+  	get ShowSilhouette(){return document.getElementById("showSilhouette").checked;},
+  	get OutputCanvas(){return document.getElementById("silhouetteCanvas");},
+  	get SilhouetteScale(){return document.getElementById("silhouetteScale").value / 100;},
+  	get ForegroundScale(){return document.getElementById("foregroundScale").value / 100;}
+  }
+
   let lastSearch = {};
+  let MouseOver = null;
 
   library.backgroundData = {};
   library.maskData = {};
+
+  library.fillcard;
   library.cardArray = [];
 
   library.SelectionCallBack;
 
 
-  let applyXOffset = function(){return document.getElementById("xHalfCard").checked};
-  let applyYOffset = function(){return document.getElementById("yHalfCard").checked};
   let cardImageOutput = function(){return document.getElementById("findCards");};
-  let showForground = function(){return document.getElementById("showForeground").checked};
-  let showSilhouette = function(){return document.getElementById("showSilhouette").checked};
 
-	function GetCardData(cardName,callBack)
+  function GetCardData(cardName,callBack)
   {
   	var xhttp = new XMLHttpRequest();
   	xhttp.onreadystatechange = function()
@@ -33,19 +50,29 @@ var PokeSilhouette = (function() {
   	xhttp.send();
   }
   
-  library.DrawCardArray = function()
+  library.SetMouseOver = function(location)
   {
-      let xStart = (applyXOffset() ? - cardWidth/2 : 0);
-      let yStart = (applyYOffset() ? - cardHeight/2 : 0);
+  	MouseOver = (location != null ? {x : location[0] * CardSize.DrawSize.Width, y : location[1] * CardSize.DrawSize.Height} : null);  	
+  }
+
+  function DrawCardArray()
+  {
+      let xStart = 0;
+      let yStart = 0;
 
       let c = document.createElement('canvas');
-      c.width = 800;
-      c.height = 800;
+      c.width = UI.OutputCanvas.width;
+      c.height = UI.OutputCanvas.height;
 
       let ctx = c.getContext("2d");
-      for(let y = 0; y < library.cardArray.length; y++)
-          for(let x = 0; x < library.cardArray[y].length; x++)
-              ctx.drawImage(library.cardArray[y][x], xStart + x*cardWidth, yStart + y*cardHeight,cardWidth,cardHeight);    
+      for(let y = 0; y < UI.CardsHeigh; y++)
+      {
+          for(let x = 0; x < UI.CardsWide; x++)
+          {
+          	let image = library.cardArray[y][x] != null ? library.cardArray[y][x] : library.fillcard;
+            ctx.drawImage(image, xStart + x * CardSize.DrawSize.Width, yStart + y * CardSize.DrawSize.Height, CardSize.DrawSize.Width, CardSize.DrawSize.Height);
+          }
+      }
 
       return c;
   }
@@ -53,17 +80,17 @@ var PokeSilhouette = (function() {
   library.DrawSilhouette = function()
   {
     let c = document.createElement('canvas');
-    c.width = 800;
-    c.height = 800;
+    c.width = UI.OutputCanvas.width;
+    c.height = UI.OutputCanvas.height;
 
     let ctx=c.getContext("2d");
     ctx.clearRect(0,0,c.width,c.height);
  
-    let cards = PokeSilhouette.DrawCardArray();
+    let cards = DrawCardArray();
 
-    if(showSilhouette())
+    if(UI.ShowSilhouette)
     {
-      ctx.drawImage(library.maskData.image, 0, 0, 600 , 530);
+      ctx.drawImage(library.maskData.image, 0, 0, library.maskData.image.width * UI.SilhouetteScale, library.maskData.image.height * UI.SilhouetteScale);
       ctx.globalCompositeOperation = 'source-in';
     }
     ctx.drawImage(cards, 0 , 0);
@@ -74,23 +101,39 @@ var PokeSilhouette = (function() {
   library.DoDraw = function()
   {
     let c = document.createElement('canvas');
-    c.width = 800;
-    c.height = 800;
+    c.width = UI.OutputCanvas.width;
+    c.height = UI.OutputCanvas.height;
 
     let ctx = c.getContext("2d");
-    ctx.clearRect(0,0,c.width,c.height);
- 
+
     let img = PokeSilhouette.DrawSilhouette();
 
-    if(showForground())
-      ctx.drawImage(library.backgroundData.image , 0 ,0 );
+    if(UI.ShowForground)
+      ctx.drawImage(library.backgroundData.image , 0 ,0, library.backgroundData.image.width * UI.ForegroundScale, library.backgroundData.image.height * UI.ForegroundScale);
 
-    ctx.drawImage(img, 150 , 150);
+    ctx.drawImage(img, UI.CardsxOffest , UI.CardsyOffest);
 
-    return c;
+    OutputImage(c);    
   }
 
-	library.FindCards = function(cardName)
+  function OutputImage(img)
+  {
+  	let c = UI.OutputCanvas;
+    let ctx = c.getContext("2d");
+    ctx.clearRect(0,0,c.width,c.height);
+    ctx.drawImage(img, 0 , 0);
+
+    if(MouseOver != null)
+    {
+		  ctx.fillStyle = '#0000005e';		
+    	ctx.strokeStyle = '#ffffff';
+
+    	ctx.fillRect((parseInt(MouseOver.x) + parseInt(UI.CardsxOffest)), MouseOver.y, CardSize.DrawSize.Width, CardSize.DrawSize.Height);
+    	ctx.strokeRect((parseInt(MouseOver.x) + parseInt(UI.CardsxOffest)), MouseOver.y, CardSize.DrawSize.Width ,CardSize.DrawSize.Height);
+    }
+  }
+
+  library.FindCards = function(cardName)
   {
 		  GetCardData(cardName,function(data){        
 
@@ -109,6 +152,11 @@ var PokeSilhouette = (function() {
         });
       });
   }
+
+
+
+  for(let y = 0; y < 5; y++)
+    library.cardArray.push([null,null,null,null,null,null,null]);
 
   return library;
   
